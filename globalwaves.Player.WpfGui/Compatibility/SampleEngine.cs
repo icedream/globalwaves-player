49c31684e2c7bf2aaaffba47d44fe6a562409fa5
Icedream <icedream2k9@die-optimisten.net>
2012-12-02 21:46:28 +0100

    Added missing files. ._.

---
+++ globalwaves.Player.WpfGui/Compatibility/SampleEngine.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Threading;
+using NAudio.Wave;
+using WPFSoundVisualizationLib;
+
+namespace globalwaves.Player.WpfGui.Compatibility
+{
+    class SampleEngine : ISpectrumPlayer, IDisposable, INotifyPropertyChanged
+    {
+        #region Fields
+        private static SampleEngine instance;
+        private readonly BackgroundWorker waveformGenerateWorker = new BackgroundWorker();
+        private readonly int fftDataSize = (int)FFTDataSize.FFT2048;
+        private SampleAggregator sampleAggregator = new SampleAggregator((int)FFTDataSize.FFT2048);
+        #endregion
+
+        public void AddSample(float sampleL, float sampleR)
+        {
+            sampleAggregator.Add(sampleL, sampleR);
+        }
+
+        #region Constants
+        private const int waveformCompressedPointCount = 2000;
+        #endregion
+
+        #region Singleton Pattern
+        public static SampleEngine Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SampleEngine();
+                return instance;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        private SampleEngine()
+        {
+        }
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+        #endregion
+
+        #region IDisposable
+        private bool disposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                disposed = true;
+            }
+        }
+        #endregion
+
+        #region ISoundPlayer
+
+        private bool _isPlaying = false;
+        public bool IsPlaying { get { return _isPlaying; } set { _isPlaying = value; NotifyPropertyChanged("IsPlaying"); } }
+
+        #endregion
+
+        #region ISpectrumPlayer
+
+        public bool GetFFTData(float[] fftDataBuffer)
+        {
+            sampleAggregator.GetFFTResults(fftDataBuffer);
+            return IsPlaying;
+        }
+
+        public int GetFFTFrequencyIndex(int frequency)
+        {
+            double maxFrequency;
+            //if (ActiveStream != null)
+            //    maxFrequency = ActiveStream.WaveFormat.SampleRate / 2.0d;
+            //else
+                maxFrequency = 22050; // Assume a default 44.1 kHz sample rate.
+            return (int)((frequency / maxFrequency) * (fftDataSize / 2));
+        }
+
+        #endregion
+    }
+}
---
+++ globalwaves.Player.WpfGui/Gui/MarqueeContentPanel.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace globalwaves.Player.WpfGui.Gui
+{
+    /// <summary>
+    /// Interaktionslogik für MarqueeContentPanel.xaml
+    /// </summary>
+    public partial class MarqueeContentPanel : UserControl
+    {
+        DoubleAnimation _doubleAnimation;
+        Storyboard _storyBoard;
+
+        public MarqueeContentPanel()
+        {
+            InitializeComponent();
+
+            _doubleAnimation = new DoubleAnimation();
+            _doubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            _doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(5));
+            _doubleAnimation.EasingFunction = new QuadraticEase();
+            ((QuadraticEase)_doubleAnimation.EasingFunction).EasingMode = EasingMode.EaseInOut;
+            _storyBoard = new Storyboard();
+            _storyBoard.Children.Add(_doubleAnimation);
+            Storyboard.SetTarget(_doubleAnimation, this.title);
+            Storyboard.SetTargetProperty(_doubleAnimation, new PropertyPath(Canvas.LeftProperty));
+
+            Title = "";
+            Artist = "";
+        }
+
+        public Duration Duration
+        { get; set; }
+
+        public bool ShowLoader
+        { get { return this.fluidProgressBar1.Opacity > 0; } set { this.fluidProgressBar1.Opacity = value ? 1 : 0; this.artist.Opacity = this.title.Opacity = value ? 0 : 1; } }
+
+        public string Title
+        {
+            get { return title.Content.ToString(); }
+            set
+            {
+                _storyBoard.Stop();
+                _storyBoard.Seek(TimeSpan.FromTicks(0));
+                title.Content = value;
+                if (title.ActualWidth < this.ActualWidth)
+                {
+                    return;
+                }
+                else
+                {
+                    //_doubleAnimation.From = this.ActualWidth;
+                    //_doubleAnimation.To = -title.ActualWidth;
+                    _doubleAnimation.From = 0;
+                    _doubleAnimation.To = -title.ActualWidth + this.ActualWidth;
+                    _storyBoard.Begin();
+                }
+            }
+        }
+
+        public string Artist
+        { get { return artist.Content.ToString(); } set { artist.Content = value; } }
+
+    }
+}
